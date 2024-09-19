640fe09003961c8a211609d4b5d22977a6a5c744
hatrungnguyen <hatrungnguyen04@gmail.com>
2024-09-19 22:26:38 +0700

    login and menu

--- app/src/main/java/vn/edu/usth/flightinfoapp/LoginFragment.java
+++ app/src/main/java/vn/edu/usth/flightinfoapp/LoginFragment.java
@@ -1,64 +1,62 @@
 package vn.edu.usth.flightinfoapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import vn.edu.usth.flightinfoapp.fragment.MenuFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        usernameEditText = view.findViewById(R.id.username);
+        passwordEditText = view.findViewById(R.id.password);
+        loginButton = view.findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Logged in successfully", Toast.LENGTH_SHORT).show();
+
+                    // Transition to MenuFragment after login
+                    MenuFragment menuFragment = new MenuFragment();
+                    Bundle args = new Bundle();
+                    args.putString("username", username.trim());
+                    menuFragment.setArguments(args);
+
+                    if (getActivity() != null) {
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, menuFragment) // Use fragment_container ID
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/vn/edu/usth/flightinfoapp/fragment/ViewPagerAdapter.java
+++ app/src/main/java/vn/edu/usth/flightinfoapp/fragment/ViewPagerAdapter.java
@@ -1,4 +1,36 @@
 package vn.edu.usth.flightinfoapp.fragment;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import vn.edu.usth.flightinfoapp.fragment.HomeFragment;
+import vn.edu.usth.flightinfoapp.fragment.NotiFragment;
+import vn.edu.usth.flightinfoapp.fragment.ProfileFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new NotiFragment();
+            case 2:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3; // Number of fragments
+    }
 }
