6af26162522a7729625f48512bc2d0b692081856
QuyTungDao <tbbuong@gmail.com>
2024-11-08 22:16:19 +0700

    Task

--- app/src/main/java/vn/edu/usth/flightinfoapp/tasks/ConnectionAsyncTask.java
+++ app/src/main/java/vn/edu/usth/flightinfoapp/tasks/ConnectionAsyncTask.java
@@ -1,4 +1,37 @@
 package vn.edu.usth.flightinfoapp.tasks;
 
-public class ConnectionAsyncTask {
+import android.os.AsyncTask;
+import android.util.Log;
+
+import vn.edu.usth.flightinfoapp.api.HttpManager;
+
+public class ConnectionAsyncTask extends AsyncTask<String, String, String> {
+    private static final String TAG = "ConnectionAsyncTask";
+    private TaskCallback taskCallback;
+
+    public ConnectionAsyncTask(TaskCallback taskCallback) {
+        this.taskCallback = taskCallback;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String result = HttpManager.getData(params[0]);
+        return result != null && !result.isEmpty() ? result : null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (result != null) {
+            taskCallback.onTaskSuccess(result);
+            Log.d(TAG, "Task succeeded: " + result);
+        } else {
+            taskCallback.onTaskFailure();
+        }
+    }
+
+    public interface TaskCallback {
+        void onTaskSuccess(String result);
+
+        void onTaskFailure();
+    }
 }
--- app/src/main/java/vn/edu/usth/flightinfoapp/tasks/FlightJsonParser.java
+++ app/src/main/java/vn/edu/usth/flightinfoapp/tasks/FlightJsonParser.java
@@ -1,4 +1,34 @@
 package vn.edu.usth.flightinfoapp.tasks;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.usth.flightinfoapp.model.Flight;
+
 public class FlightJsonParser {
+    private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static List<Flight> getObjectFromJson(String json) {
+        List<Flight> flights = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                Flight flight = new Flight();
+
+                flights.add(flight);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return flights;
+    }
 }
