d84a978236888cd3309b36e8d81dc357a63938af
NguyenTriAnhVu <nguyenvutri2206@gmail.com>
2024-11-08 22:14:55 +0700

    ultis update

--- app/src/main/java/vn/edu/usth/flightinfoapp/utils/CustomArrayAdapter.java
+++ app/src/main/java/vn/edu/usth/flightinfoapp/utils/CustomArrayAdapter.java
@@ -1,4 +1,54 @@
 package vn.edu.usth.flightinfoapp.utils;
 
-public class CustomArrayAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+
+import java.util.List;
+
+import vn.edu.usth.flightinfoapp.R;
+
+public class CustomArrayAdapter extends ArrayAdapter<String> {
+    private float fontSize;
+
+    public CustomArrayAdapter(Context context, int resource, List<String> objects, float fontSize) {
+        super(context, resource, objects);
+        this.fontSize = fontSize;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_item, parent, false);
+        }
+        TextView textView = convertView.findViewById(R.id.text1);
+        if (textView != null) {
+            textView.setTextSize(fontSize);
+            textView.setTypeface(ResourcesCompat.getFont(getContext(), R.font.poppins_medium));
+            textView.setText(getItem(position));
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_item, parent, false);
+        }
+        TextView textView = convertView.findViewById(R.id.text1);
+        if (textView != null) {
+            textView.setTextSize(fontSize);
+            textView.setTypeface(ResourcesCompat.getFont(getContext(), R.font.poppins_medium));
+            textView.setText(getItem(position));
+            textView.setPadding(16, 4, 16, 4);
+        }
+        return convertView;
+    }
 }
